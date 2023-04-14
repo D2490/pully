f4bfac77ea014a71bcec97536a932fd3568cd7de
D2490 <103814015+D2490@users.noreply.github.com>
2023-04-15 02:39:07 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,12 @@
-// TODO: implement the tests
+const index = require("../index")
 console.log("Tests are not implemented yet...");
+
+index.ls("/imagegeneration");
+console.log("Tests are not implemented yet...");
+
+index.ls("/data");
+console.log("Tests are not implemented yet...");
+
+index.ls("/");
+index.cat("./index.js")
+setTimeout(() => { console.log("World!"); }, 5000);
